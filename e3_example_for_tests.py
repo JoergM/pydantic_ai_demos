2eb01fd0b2b074fbe874d72df04428fdf9d1da2b
Joerg Mueller <JoergM@users.noreply.github.com>
2025-10-03 19:39:16 +0200

    Implemented Dependencys Example and TestModel Example.

---
+++ e3_example_for_tests.py
@@ -0,0 +1,63 @@
+import os
+
+import duckdb
+from dataclasses import dataclass
+import logfire
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from pydantic_ai import Agent
+from pydantic_ai import RunContext
+from pydantic_ai.models.openai import OpenAIChatModel
+from pydantic_ai.providers.openai import OpenAIProvider
+
+# setup logging
+load_dotenv()
+logfire.configure(token=os.getenv("LOGFIRE_TOKEN"))
+logfire.instrument_pydantic_ai()
+
+# define the output model
+class Capital(BaseModel):
+    name: str = Field(..., description="The name of the capital")
+    country: str = Field(..., description="The country of the capital, as it was asked by the user.")
+    num_inhabitants: float = Field(..., description="The number of inhabitants of the capital")
+    size_of_capital: float = Field(..., description="The size of the capital in square kilometers")
+
+# the Type defining runtime dependencies for the agent
+@dataclass
+class CapitalDeps:
+    db: duckdb.DuckDBPyConnection
+
+def init_db():
+    """initialize the database based on the csv file"""
+    db = duckdb.connect(':memory:')
+    db.execute('CREATE TABLE capitals_wikipedia '
+               'AS SELECT * from "data/capitals_wikipedia.csv"')
+    return db
+
+#this time not decorator
+def query_numbers(ctx: RunContext[CapitalDeps], capital_name: str) -> {}:
+    """query the number of inhabitants and size for a capital"""
+    row = ctx.deps.db.execute(f"SELECT inhabitants, size "
+                              f"FROM capitals_wikipedia "
+                              f"WHERE name = '{capital_name}'").fetchone()
+    if row:
+        return {'inhabitants': row[0], 'size': row[1]}
+    else:
+        return {}
+
+def get_capital_agent() -> Agent:
+    BASE_URL = "http://127.0.0.1:1234/v1"
+    LM_STUDIO_MODEL = "openai/gpt-oss-20b"
+    model = OpenAIChatModel(LM_STUDIO_MODEL, provider=OpenAIProvider(BASE_URL))
+    agent = Agent(model,
+                  output_type=Capital,
+                  deps_type=CapitalDeps, # Here we define the type of deps used in all tool calls
+                  tools=[query_numbers]) # This is an alternative way to provide tools
+    return agent
+
+if __name__ == "__main__":
+    agent = get_capital_agent()
+    deps = CapitalDeps(db=init_db()) #We need to initialize the runtime dependencies
+    result = agent.run_sync("What is the size of the capital of Germany?", deps=deps)
+    print(result.output)
+
---
+++ e3_example_test.py
@@ -0,0 +1,39 @@
+import duckdb
+import pytest
+from pydantic_ai import models
+from pydantic_ai.models.test import TestModel
+
+from e3_example_for_tests import get_capital_agent, CapitalDeps
+
+@pytest.fixture
+def fake_db():
+    """This creates a database only for the test"""
+    db = duckdb.connect(':memory:')
+    db.execute('CREATE TABLE capitals_wikipedia (name VARCHAR, inhabitants NUMERIC, size NUMERIC)')
+    db.execute("INSERT INTO capitals_wikipedia VALUES ('Berlin', 4000000, 900)") #lets round berlin numbers up
+    return CapitalDeps(db=db)
+
+@pytest.mark.asyncio
+async def test_database_values_returned(fake_db: CapitalDeps):
+    """This is some kind of integration test to demonstrate that the tool is called and the
+    database values are returned.
+    It demonstrates the use of Dependencies to guarantee results.
+    """
+    agent = get_capital_agent()
+    result = await agent.run("What is the size of the capital of Germany?", deps=fake_db)
+    assert result.output.country == "Germany"
+    assert result.output.name == "Berlin"
+    assert result.output.num_inhabitants == 4000000
+    assert result.output.size_of_capital == 900
+
+# Demonstrate the use of TestModel
+@pytest.mark.asyncio
+async def test_alternative_model(fake_db: CapitalDeps):
+    models.ALLOW_MODEL_REQUESTS = False #block any requests to real models form happening
+    agent = get_capital_agent()
+    with agent.override(model=TestModel()):
+        result = await agent.run("What is the size of the capital of Germany?", deps=fake_db)
+        print(result.output)
+
+
+
